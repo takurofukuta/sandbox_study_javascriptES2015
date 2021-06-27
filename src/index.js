c6f6e0c0ed3da6f20a667f1cee795868c46fefed
福田 拓郎 <76578159+takurofukuta@users.noreply.github.com>
2021-06-27 13:17:42 +0900

    分割代入

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
-// const name = "たくろう";
-// const age = 28;
+// 関数・アロー関数
+
+// function func1(str) {
+//   return str;
+// }
+
+// const func2 = function (str) {
+//   return str;
+// };
+
+// const func3 = (str) => {
+//   return str;
+// };
+
+// const func4 = (str) => str;
+
+// const func5 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const func6 = (num1, num2) => num1 + num2;
+
+// console.log(func1("Yes"));
+// console.log(func2("No"));
+// console.log(func3("YesNo"));
+// console.log(func4("YesNoYes"));
+// console.log(func5(10, 20));
+// console.log(func6(10, 30));
+
+// 分割代入
+// const profile = {
+//   name: "たくろう",
+//   age: 28,
+// };
+
+// const { name, age } = profile
+
+// const message = `私の名前は${profile.name}です。年齢は${profile.age}歳です`;
 // const message = `私の名前は${name}です。年齢は${age}歳です`;
 
+// console.log(profile);
 // console.log(message);
 
-function func1(str) {
-  return str;
-}
+// const profile2 = ["たくろう", 28];
+// const [name, age] = profile2;
+// const message2 = `私の名前は${name}です。年齢は${age}歳です`;
 
-const func2 = function (str) {
-  return str;
-};
-
-const func3 = (str) => {
-  return str;
-};
-
-const func4 = (str) => str;
-
-const func5 = (num1, num2) => {
-  return num1 + num2;
-};
-
-const func6 = (num1, num2) => num1 + num2;
-
-console.log(func1("Yes"));
-console.log(func2("No"));
-console.log(func3("YesNo"));
-console.log(func4("YesNoYes"));
-console.log(func5(10, 20));
-console.log(func6(10, 30));
+// console.log(profile2);
+// console.log(message2);
